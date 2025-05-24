55a012582228363a2d107569d64742f11bbc8083
Carl0sL0pez03 <clopez5@udi.edu.co>
2025-05-24 09:09:28 -0500

    WT-1: Connecting to the api to obtain products and improving the products interface.

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.tsx";
-import { CartProvider } from "./context/CartContext.tsx";
+import { CartProvider, ProductsProvider } from "./context";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <ProductsProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductsProvider>
   </StrictMode>
 );
